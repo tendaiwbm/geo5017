2760060f62ef8cc8bca87f3a60efea65e255e587
Tendai Mbwanda <t.mbwanda@student.tudelft.nl>
2023-03-04 11:53:22 +0100

    added constant speed f(x) signature

--- hw1.py
+++ hw1.py
@@ -30,8 +30,6 @@ def dAlpha1(t,x,x_pred):
 def dAlpha0(x,x_pred):
     return x - x_pred
 
-#def dAlpha22(t,x,x_pred):
-#    return 2t*
 
 # evaluates each partial derivative (coefficient)
 # re-used for alpha2, alpha1 and alpha0
@@ -39,22 +37,23 @@ def gradient(n,dAlpha):
     return (-2) * sum(dAlpha)
 
 
-def gradient_descent(X,t,learn_rate,coeffs,pa0,pa1,pa2):
+def gradient_descent(X,t,learn_rate,coeffs,partial0,partial1,partial2):
     
     X_pred = [fX(ti,*coeffs) for ti in t]
-    partials_alpha2 = [pa2(t[i], X[i], X_pred[i]) for i,x in enumerate(X)]
-    partials_alpha1 = [pa1(t[i], X[i], X_pred[i]) for i,x in enumerate(X)]
-    partials_alpha0 = [pa0(X[i], X_pred[i]) for i,x in enumerate(X)]
     
+    partials_alpha1 = [partial1(t[i], X[i], X_pred[i]) for i,x in enumerate(X)]
+    partials_alpha0 = [partial0(X[i], X_pred[i]) for i,x in enumerate(X)]
+    partials_alpha2 = [partial2(t[i], X[i], X_pred[i]) for i,x in enumerate(X)]
+        
     print(f'''partials_aplha2\t{partials_alpha2}
 partials_alpha1\t{partials_alpha1}
 partials_alpha0\t{partials_alpha0}''')
-    
+ 
     # update model coefficients
     coeffs[0] = coeffs[0] - learn_rate * gradient(len(partials_alpha2),partials_alpha2)
     coeffs[1] = coeffs[1] - learn_rate * gradient(len(partials_alpha1),partials_alpha1)
     coeffs[2] = coeffs[2] - learn_rate * gradient(len(partials_alpha0),partials_alpha0)
-    
+
     print('coeffs\t',coeffs)
     
     X_pred = [fX(ti,*coeffs) for ti in t]
@@ -67,7 +66,7 @@ partials_alpha0\t{partials_alpha0}''')
     return loss,coeffs,X_pred
 
 
-def constant_speed(X,t,coeffs,max_iter,rate,tol):
+def constant_acceleration(X,t,coeffs,max_iter,rate,tol):
     
     losses = []
     alpha2 = None
@@ -95,33 +94,22 @@ def constant_speed(X,t,coeffs,max_iter,rate,tol):
     coeffs = [alpha2,alpha1,alpha0]
     return X_pred,coeffs,losses
  
+def constant_speed():
+    '''
+        Should perform gradient descent on the partial derivatives of
+        x(t) = a1 + a2*t as Susanne wrote in the latex doc
+        
+        Objective function
+        (xi - (a1 + a2*t))**2
 
-def constant_acc(X,t,coeffs,max_iter,rate,tol):
-    losses = []
-    alpha2 = None
-    alpha1 = None
-    alpha0 = None
-    i = 0
-
-    while i < max_iter:
-        
-        loss,new_coeffs,X_pred = gradient_descent(X,t,rate,coeffs)
-        losses.append(loss)
-        
-        deltaA2 = numpy.abs(coeffs[0] - new_coeffs[0])
-        deltaA1 = numpy.abs(coeffs[1] - new_coeffs[1])
-        deltaA0 = numpy.abs(coeffs[2] - new_coeffs[2])
-        
-        if all(delta < tol for delta in [deltaA0,deltaA1,deltaA2]) or loss < tol:
-            alpha2 = coeffs[0]
-            alpha1 = coeffs[1] 
-            alpha0 = coeffs[2]
-         
-        
-        i += 1
+        Returns
+        -------
+        Final model coefficients and mean squared errors (for n-iterations)
     
-    coeffs = [alpha2,alpha1,alpha0]
-    return X_pred,coeffs,losses
+        '''
+    
+    pass
+
 
 def main():
     
@@ -133,15 +121,18 @@ def main():
             'Y': [0,1.68,1.82,0.28,-1.51,-1.91],
             'Z': [1,2.38,2.49,2.15,2.59,4.32]}
     t = [1,2,3,4,5,6]
-    original_coeffs = [9,-20,2]  
-    coeffs = [9,-20,2]
+    original_coeffs = [9,-6,2]  
+    coeffs = [9,-6,2]
     max_iter = 10000
     rate = 0.0001
     tol = 0.01
+    total_loss = []
     for j,X in enumerate(list(data.values())):
         
         # final prediction, X_pred, obtained after minimising sum of squares and final model coefficients
-        X_pred,final_coeffs,losses = constant_speed(X,t,coeffs,max_iter,rate,tol)
+        X_pred,final_coeffs,losses = constant_acceleration(X,t,coeffs,max_iter,rate,tol)
+        total_loss.append(losses)
+        
         print('original coeffs\t',original_coeffs)
         print('final coeffs\t',final_coeffs)
         
@@ -154,6 +145,7 @@ def main():
         plt.savefig(f'output{list(data.keys())[j]}.png')
         plt.show()
     
+    print(sum([total_loss[0][-1],total_loss[1][-1],total_loss[2][-1]]))
     
     
     
